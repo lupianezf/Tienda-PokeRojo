702b5baf9cc321c7088e85980655d5d9a833a92d
lupianezf <lupianezf@gmail.com>
2026-04-22 20:15:34 -0300

    e

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://ltwuhklbftacevagylfh.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0d3Voa2xiZnRhY2V2YWd5bGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4MjAxOTksImV4cCI6MjA5MjM5NjE5OX0.JfcXW7eCIPLKLV3Xk3g26c9wZEwxD05Jsatkjo_irwE'
-)
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
